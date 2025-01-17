10feeda16d9c35b9e0c0fc30e05d90bf39984013
Stone-WYD <1174872613@qq.com>
2025-01-17 11:00:19 +0800

    maven 导入项目学习内容

--- src/main/java/com/wyd/web/arthas/TestController.java
+++ src/main/java/com/wyd/web/arthas/TestController.java
@@ -1,7 +1,10 @@
 package com.wyd.web.arthas;
 
+import com.wyd.web.common.AjaxResult;
+import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -10,9 +13,10 @@ import java.util.Map;
 /**
  * @author Stone
  * @date 2024-12-26
- * @Description:
+ * @Description: 模拟测试因高并发而引起的内存溢出问题
  */
 @RestController
+@RequestMapping("/arthas/leak")
 public class TestController {
 
     /**
@@ -24,24 +28,31 @@ public class TestController {
         Thread.sleep(10 * 1000L);
     }
 
-    private final Map<String, UserEntity> userCache = new HashMap<>();
+    private final Map<Long, UserEntity> userCache = new HashMap<>();
 
     /**
      * 登录接口 传递名字和id,放入hashmap中
      */
     @PostMapping("/login")
-    public void login(String name,String id){
+    public void login(String name,Long id){
         userCache.put(id,new UserEntity(id,name));
     }
 
-    private static class UserEntity {
-        private final byte[] bytes = new byte[1024 * 1024 * 10];
-        public UserEntity(String id, String name) {
+    @PostMapping("/feign")
+    public AjaxResult<UserEntity> feignTest(String name, Long id){
+        UserEntity data = new UserEntity(id, name);
+        System.out.println("====================== feign 被调用了 ===========================");
+        return AjaxResult.getTrueAjaxResult(data);
+    }
+
+    @Data
+    public static class UserEntity {
+        public UserEntity(Long id, String name) {
             this.id = id;
             this.name = name;
         }
 
-        private String id;
+        private Long id;
         private String name;
 
 
